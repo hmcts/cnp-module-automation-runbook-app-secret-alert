03b9d510a6586018cc77fb5a4209fa899c20ae49
Your Name <james.ferrari@version1.com>
2023-01-16 16:34:34 +0000

    initial push

---
+++ Get-AppRegistrationExpiry.ps1
@@ -0,0 +1,136 @@
+########################################################################################
+<#
+.DESCRIPTION
+    Name:           Get-AppRegistrationExpiry.ps1
+
+    Description:    Checks Azure AD App Registration's for expiring client secrets;
+                    Creates Dyanatrace alert if client secret is within 21 Days of expiring.
+
+    Author:         James Ferrari
+
+    Version:        1.0
+
+    Date:           16/01/2023
+
+    Requires:       N/A
+
+.CHANGE LOG
+    Version 1.0     16/01/2023    Initial Release
+#>
+########################################################################################
+
+[CmdletBinding()]
+param (
+    [Parameter()]
+    [string[]]
+    $applicationIds,
+
+    [Parameter()]
+    [string]
+    $azureTenant,
+
+    [Parameter()]
+    [string]
+    $azureCredential,
+
+    [Parameter()]
+    [string]
+    $dynatraceTenant,
+
+    [Parameter()]
+    [string]
+    $dynatraceCredential,
+
+    [Parameter()]
+    [string]
+    $entityName,
+
+    [Parameter()]
+    [string]
+    $entityType,
+
+    [Parameter()]
+    [string]
+    $project
+)
+
+###############################################
+# Global. #####################################
+Import-Module -Name 'Az.Accounts'
+Import-Module -Name 'Az.Resources'
+Import-Module -Name 'Az.Automation'
+
+$dateTime = (Get-Date)
+
+###############################################
+# Authenticate with Azure. ####################
+$azureCredentials = Get-AutomationPSCredential -Name $azureCredential
+$azureUser        = $azureCredentials.Username 
+$azurePass        = $azureCredentials.GetNetworkCredential().Password | ConvertTo-SecureString -AsPlainText
+$azCredential     = New-Object -TypeName System.Management.Automation.PSCredential -ArgumentList $azureUser, $azurePass
+
+Connect-AzAccount -ServicePrincipal -TenantId $azureTenant -Credential $azCredential | Out-Null
+
+###############################################
+# Check App Registration Expirys. #############
+foreach ($id in $applicationIds) {
+  $appName = (Get-AzADApplication -Filter "AppId eq '$id'").displayName
+  Write-Output "Checking Secret Expiration for App Registration : $appName"
+
+  # Get App Registration Client Secrets.
+  $credentials      = (Get-AzADAppCredential -ApplicationId $id)
+  $credentialsCount = $credentials.Count
+  Write-Output "`tFound $credentialsCount Client Secrets."
+
+  ########################################
+  # Check Credentials. ###################
+  $counter = 0
+
+  foreach ($cred in $credentials) {
+
+    $expiryDate = (Get-Date $cred.endDateTime)
+    $notifyDate = (Get-Date $expiryDate).AddDays(-21)
+
+    if ($dateTime -gt $notifyDate ) {
+      $counter++
+    }
+  }
+
+  ###########################################
+  # Create Dynatrace Alert. #################
+  if ($counter -gt 0) {
+    Write-Output "`tFound $counter Client Secret Due to Expire Soon, Creating Dynatrace Alert..."
+
+    # Get Credentials.
+    $dynatraceCredentials = Get-AutomationPSCredential -Name $dynatraceCredential
+    $apiToken             = $dynatraceCredentials.GetNetworkCredential().Password
+
+    # Create Headers.
+    $requestHeaders = New-Object "System.Collections.Generic.Dictionary[[String],[String]]"
+    $requestHeaders.Add("Authorization", "API-Token $apiToken")
+    $requestHeaders.Add("Content-Type", "application/json")
+
+    # Create Body.
+    $requestBody = [PSCustomObject]@{
+      "eventType"      = "ERROR_EVENT"
+      "title"          = "FH - $project - App Registration Client Secret Expiry $appName"
+      "entitySelector" = "type($entityType),entityName.startsWith($entityName)"
+      "properties"     = @{
+          "Tenant ID"      = "$azureTenant"
+          "Application ID" = "$id"
+          "Display Name"   = "$appName"
+          "Description"    = "App Registration $appName Has $counter Client Secrets Due To Expire Soon."
+      }
+    } | ConvertTo-Json
+
+    # POST Data to Dynatrace.
+    Write-Output "`tCreating Alert in Dynatrace, Please Wait..."
+    Write-Output "`n"
+
+    $response = Invoke-RestMethod "https://$dynatraceTenant.live.dynatrace.com/api/v2/events/ingest" -Method 'POST' -Headers $requestHeaders -Body $requestBody
+    $response | ConvertTo-Json
+  }
+  else {
+    Write-Output "`tFound 0 Client Secrets That Are Due To Expire Soon."
+  }
+}
--- README.md
+++ README.md
@@ -1,1 +1,64 @@
-# cnp-module-automation-runbook-app-secret-alert
+## Dynatrace Module
+
+This Module will create a PowerShell runbook which checks for App Registration Client Secrets that are expiring within 21 days.
+
+```terraform
+module "app_secret_expiry" {
+  source = "git::https://github.com/hmcts/cnp-module-automation-runbook-app-secret-alert"
+
+  automation_account_name = "sds-aa"
+  resource_group_name     = "sds-rg
+  location                = "uksouth"
+  credentials             = [
+                                {
+                                    name        = "azure-ad-credential"
+                                    description = "a credential for azure ad access"
+                                    username    = "00000-000000-00000" # app registration id which has access to query azure ad.
+                                    password    = data.azurerm_key_vault_secret.password.value # app registration client secret.
+                                }
+                            ]
+
+  tags                    = var.common_tags
+}
+
+```
+
+## Requirements   
+
+| Name | Version |
+|------|---------|
+| <a name="requirement_terraform"></a> [terraform](#requirement\_terraform) | >= 1.3.7 |
+
+## Providers
+
+| Name | Version |
+|------|---------|
+| <a name="provider_azurerm"></a> [azurerm](#provider\_azurerm) | 3.38.0 |
+
+## Modules
+
+No modules.
+
+## Resources
+
+| Name | Type |
+|------|------|
+| [azurerm_automation_credential.credential](https://registry.terraform.io/providers/hashicorp/azurerm/latest/docs/resources/automation_credential) | resource |
+| [azurerm_automation_runbook.new_dynatrace_alert](https://registry.terraform.io/providers/hashicorp/azurerm/latest/docs/resources/automation_runbook) | resource |
+| [azurerm_automation_account.automation_account](https://registry.terraform.io/providers/hashicorp/azurerm/latest/docs/data-sources/automation_account) | data source |
+
+## Inputs
+
+| Name | Description | Type | Default | Required |
+|------|-------------|------|---------|:--------:|
+| <a name="input_automation_account_name"></a> [automation\_account\_name](#input\_automation\_account\_name) | name of automation account | `string` | n/a | yes |
+| <a name="input_automation_credentials"></a> [automation\_credentials](#input\_automation\_credentials) | service principal credentials which have access to query azure ad | `list(map(string))` | `[]` | no |
+| <a name="input_location"></a> [location](#input\_location) | azure location | `string` | n/a | yes |
+| <a name="input_log_progress"></a> [log\_progress](#input\_log\_progress) | choose whether the runbook is to log progress | `bool` | `false` | no |
+| <a name="input_log_verbose"></a> [log\_verbose](#input\_log\_verbose) | choose whether the runbook is to log verbose | `bool` | `false` | no |
+| <a name="input_resource_group_name"></a> [resource\_group\_name](#input\_resource\_group\_name) | name of resource group | `string` | n/a | yes |
+| <a name="input_tags"></a> [tags](#input\_tags) | tags | `map(string)` | n/a | yes |
+
+## Outputs
+
+N/A
